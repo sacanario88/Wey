eb0f5dd65d4091d8b49a7628e014336dc8419138
sacanario88 <sacanario88@gmail.com>
2026-05-12 18:42:53 +0100

    Add files via upload

---
+++ Js/iframe.js
@@ -0,0 +1,10 @@
+(function(){
+
+    // 🔤 CREAMOS CAJA DE IFRAME
+    let tv = document.createElement("iframe");
+    tv.src = 'https://sacanario88.github.io/Wey/zenjava.html';
+    tv.id = 'tv';
+    tv.height='600'
+    tv.style = "width:90%; margin:5px; padding:8px; border-radius:4px; border:1px solid #ccc;";
+    document.body.appendChild(tv);
+})();
