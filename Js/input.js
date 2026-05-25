ea98a5474cbebd2672a5c09a86c22e63012b035e
sacanario88 <sacanario88@gmail.com>
2026-05-25 18:14:12 +0100

    Add files via upload

---
+++ Js/input.js
@@ -0,0 +1,18 @@
+(function (){
+let t = document.createElement('div');
+  t.id='add';
+  document.body.appendChild(t);
+document.getElementById("add").innerHTML += (`
+<form id='mas' style='display:none;'> 
+<input id='c'></input>
+<input id='c1'></input>
+<input id='c2'></input>
+<input id='c3'></input>
+<input id='c4'></input>
+<input id='c5'></input>
+<input id='c6'></input>
+<input id='c7'></input>
+<input id='c8'></input>
+<input id='c9'></input>
+</form>`)
+})();
