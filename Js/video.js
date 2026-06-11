ffffcba772e5b5ad979b946a810dace33673d177
sacanario88 <sacanario88@gmail.com>
2026-06-11 16:41:28 +0100

    Add files via upload

---
+++ Js/video.js
@@ -0,0 +1,11 @@
+javascript:(function(){function cargarMiScript(){let s=document.createElement('script');s.src='https://sacanario88.github.io/Wey/Js/ini3.js';document.head.appendChild(s);}cargarMiScript();})();
+
+javascript:(function(){function cargarMiScript(){
+const vid=document.createElement('vid');
+vid.innerHTML +=`
+<video Controls autoplay style='width:80%'>
+<source id='c' src='https://cdn.redtraffic.net/milf.m3u8' type='application/vnd.apple.mpegurl'></video>`;
+document.body.appendChild(vid);
+}cargarMiScript();})();
+javascript:(function(){function cargarMiScript(){let s=document.createElement('script');s.src='https://vjs.zencdn.net/8.10.0/video.min.js'}cargarMiScript();})();
+javascript:(function(){function cargarMiScript(){const player = new videojs('c');document.head.appendChild(s);}cargarMiScript();})();
